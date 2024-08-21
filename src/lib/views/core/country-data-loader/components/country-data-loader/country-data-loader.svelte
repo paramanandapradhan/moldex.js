c2fb3a1d95688caecf40378a70cad6af6617dd3c
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-08-21 09:19:48 +0530

    Country data loader is added

---
+++ src/lib/views/core/country-data-loader/components/country-data-loader/country-data-loader.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import EasyScriptLoader from "@cloudparker/easy-script-loader-svelte";
+    export let scriptUrl: string =
+		'https://cdn.jsdelivr.net/gh/paramanandapradhan/easy-countrydata@main/dist/index.js';
+
+
+</script>
+
+<EasyScriptLoader scriptName="EasyCountryData" {scriptUrl} on:load={() => console.log('Script loaded')} />
+
+
---
+++ src/lib/views/core/country-data-loader/index.ts
@@ -0,0 +1,5 @@
+import CountryDataLoader from "./components/country-data-loader/country-data-loader.svelte";
+
+export{
+    CountryDataLoader
+}
