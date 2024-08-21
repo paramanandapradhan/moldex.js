c3cb3fb242ae4ce57b6e12842cf45066a79d61bd
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-08-21 17:03:27 +0530

    Added Lazy image

---
+++ src/lib/views/core/lazy-image/components/lazy-image/lazy-image.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	type PropsType = {
+		src?: string;
+		imgClass?: string;
+		imgStyle?: string;
+		children?: Snippet;
+	};
+	let { src = '', imgClass = '', imgStyle = '', children }: PropsType = $props();
+</script>
+
+<img
+	transition:fade
+	{src}
+	alt="Post"
+	loading="lazy"
+	class={`max-w-full max-h-full bg-gray-200 transition-opacity ease-in-out duration-1000 ${imgClass}`}
+	style={imgStyle}
+/>
---
+++ src/lib/views/core/lazy-image/index.ts
@@ -0,0 +1,5 @@
+import LazyImage from "./components/lazy-image/lazy-image.svelte";
+
+export {
+    LazyImage
+}
