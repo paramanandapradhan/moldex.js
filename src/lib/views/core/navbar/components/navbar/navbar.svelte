ba4f87f0de9d905841bc003f51d3db68092e8276
Param <param@mac.com>
2024-09-05 21:43:31 +0530

    Updated navbar

--- src/lib/views/core/navbar/components/navbar/navbar.svelte
+++ src/lib/views/core/navbar/components/navbar/navbar.svelte
@@ -5,61 +5,63 @@
 	import { Icon, mdiMenu } from '$lib/views/core/icon';
 
 	type PropsType = {
-		id?: string;
-		className?: String;
-		hasDrawer?: boolean;
-		drawerIconPath?: string;
-		drawerButtonClassName?: string;
-		logoButtonClassName?: string;
-		hasLogo?: boolean;
-		logoIconPath?: string;
-		logoIconClassName?: string;
-		logoImgSrc?: string;
-		logoImgClassName?: string;
-		leftContainerClassName?: string;
 		centerContainerClassName?: string;
-		rightContainerClassName?: string;
-		title?: string;
-		subtitle?: string;
-		titleClassName?: string;
-		subtitleClassName?: string;
-		hasTitle?: boolean;
-		hasSubtitle?: boolean;
-		children?: Snippet;
-		leftSnippet?: Snippet;
 		centerSnippet?: Snippet;
-		rightSnippet?: Snippet;
-		onDrawer?: () => void;
+		children?: Snippet;
+		className?: String;
+		drawerButtonClassName?: string;
+		hasLogo?: boolean;
+		hasMore?: boolean;
+		hasSubtitle?: boolean;
+		hasTitle?: boolean;
+		id?: string;
+		leftContainerClassName?: string;
+		leftSnippet?: Snippet;
+		logoButtonClassName?: string;
+		logoIconClassName?: string;
+		logoIconPath?: string;
+		logoImgClassName?: string;
+		logoImgSrc?: string;
+		moreIconPath?: string;
+		morePosition?: 'left' | 'right';
 		onLogo?: () => void;
+		onMore?: () => void;
+		rightContainerClassName?: string;
+		rightSnippet?: Snippet;
+		subtitle?: string;
+		subtitleClassName?: string;
+		title?: string;
+		titleClassName?: string;
 	};
 
 	let {
-		id = '',
-		children,
-		leftSnippet,
-		centerSnippet,
-		rightSnippet,
-		className = '',
-		drawerIconPath = mdiMenu,
-		drawerButtonClassName = '',
-		hasDrawer = false,
-		hasLogo = false,
-		logoButtonClassName = '',
-		logoIconPath,
-		logoIconClassName = 'text-indigo-600',
-		logoImgSrc,
-		logoImgClassName = '',
-		leftContainerClassName = '',
 		centerContainerClassName = '',
-		rightContainerClassName = '',
-		title = '',
-		subtitle = '',
-		titleClassName = '',
-		subtitleClassName = '',
-		hasTitle = false,
+		centerSnippet,
+		children,
+		className = '',
+		drawerButtonClassName = '',
+		hasLogo = false,
+		hasMore = false,
 		hasSubtitle = false,
-		onDrawer,
-		onLogo
+		hasTitle = false,
+		id = '',
+		leftContainerClassName = '',
+		leftSnippet,
+		logoButtonClassName = '',
+		logoIconClassName = 'text-indigo-600',
+		logoIconPath,
+		logoImgClassName = '',
+		logoImgSrc,
+		moreIconPath = mdiMenu,
+		morePosition = 'left',
+		onLogo,
+		onMore,
+		rightContainerClassName = '',
+		rightSnippet,
+		subtitle = '',
+		subtitleClassName = '',
+		title = '',
+		titleClassName = ''
 	}: PropsType = $props();
 </script>
 
@@ -69,46 +71,62 @@
 	{#if children}
 		{@render children()}
 	{:else}
-		{#if hasDrawer}
-			<Button
-				className="h-full px-2 text-gray-500 hover:text-gray-600 {drawerButtonClassName}"
-				iconPath={drawerIconPath}
-				disabled={!onDrawer}
-				onClick={onDrawer}
-			></Button>
+		{#if hasMore && morePosition === 'left'}
+			<div class="h-full">
+				<Button
+					className="h-full px-2 text-gray-500 hover:text-gray-600 {drawerButtonClassName}"
+					iconPath={moreIconPath}
+					disabled={!onMore}
+					onClick={onMore}
+				/>
+			</div>
 		{/if}
 		{#if hasLogo}
-			<Button className="h-full px-2 {logoButtonClassName}" disabled={!onLogo} onClick={onLogo}>
-				{#if logoIconPath}
-					<Icon path={logoIconPath} className="h-10 w-10 {logoIconClassName}" />
+			<div class="h-full">
+				<Button className="h-full px-2 {logoButtonClassName}" disabled={!onLogo} onClick={onLogo}>
+					{#if logoIconPath}
+						<Icon path={logoIconPath} className="h-10 w-10 {logoIconClassName}" />
+					{/if}
+					{#if logoImgSrc}
+						<img src={logoImgSrc} class="h-8 w-8 bg-gray-50 {logoImgClassName}" alt="logo" />
+					{/if}
+				</Button>
+			</div>
+		{/if}
+		<div class="h-full flex items-center">
+			<div>
+				{#if hasTitle}
+					<div class="font-bold px-2 {titleClassName}">{title || ''}</div>
 				{/if}
-				{#if logoImgSrc}
-					<img src={logoImgSrc} class="h-8 w-8 bg-gray-50 {logoImgClassName}" alt="logo" />
+				{#if hasSubtitle}
+					<div class="text-sm px-2 text-gray-400 {subtitleClassName}">{subtitle || ''}</div>
 				{/if}
-			</Button>
+			</div>
+		</div>
+		<div class="h-full {leftContainerClassName}">
+			{#if leftSnippet}
+				{@render leftSnippet()}
+			{/if}
+		</div>
+		<div class="flex-grow flex items-center justify-center h-full {centerContainerClassName}">
+			{#if centerSnippet}
+				{@render centerSnippet()}
+			{/if}
+		</div>
+		<div class="h-full {rightContainerClassName}">
+			{#if rightSnippet}
+				{@render rightSnippet()}
+			{/if}
+		</div>
+		{#if hasMore && morePosition === 'right'}
+			<div class="h-full">
+				<Button
+					className="h-full px-2 text-gray-500 hover:text-gray-600 {drawerButtonClassName}"
+					iconPath={moreIconPath}
+					disabled={!onMore}
+					onClick={onMore}
+				/>
+			</div>
 		{/if}
 	{/if}
-	<div>
-		{#if hasTitle}
-			<div class="font-bold px-2 {titleClassName}">{title || ''}</div>
-		{/if}
-		{#if hasSubtitle}
-			<div class="text-sm px-2 text-gray-400 {subtitleClassName}">{subtitle || ''}</div>
-		{/if}
-	</div>
-	<div class={leftContainerClassName}>
-		{#if leftSnippet}
-			{@render leftSnippet()}
-		{/if}
-	</div>
-	<div class="flex-grow flex justify-center {centerContainerClassName}">
-		{#if centerSnippet}
-			{@render centerSnippet()}
-		{/if}
-	</div>
-	<div class={rightContainerClassName}>
-		{#if rightSnippet}
-			{@render rightSnippet()}
-		{/if}
-	</div>
 </nav>
