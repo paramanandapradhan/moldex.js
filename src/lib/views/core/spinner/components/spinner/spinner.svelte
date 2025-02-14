130cb4f42a2e585cbcbb892e56274f71d407b0c5
Param <param@mac.com>
2025-02-14 21:25:09 +0530

    Updated Spinner color

--- src/lib/views/core/spinner/components/spinner/spinner.svelte
+++ src/lib/views/core/spinner/components/spinner/spinner.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <svg
-	class="animate-spin h-5 w-5 text-gray-900 {className}"
+	class="animate-spin h-5 w-5 text-primary {className}"
 	xmlns="http://www.w3.org/2000/svg"
 	fill="none"
 	viewBox="0 0 24 24"
